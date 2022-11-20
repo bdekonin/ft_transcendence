1205f3e81a7dc2bc97e88d3cfe222635e4cac0ac
rkieboom <rkieboom@student.codam.nl>
2022-11-20 19:49:26 +0100

    Init new home screen

---
+++ frontend/src/Home/Home.tsx
@@ -0,0 +1,22 @@
+import './style.css';
+
+const Home: React.FC = () => {
+
+	return (
+		<>
+			<div className='navigator'>
+				<button></button>
+				<br />
+				<button></button>
+				<br />
+				<button></button>
+				<br />
+				<button></button>
+				<br />
+			</div>
+		</>
+	);
+
+}
+
+export default Home;
