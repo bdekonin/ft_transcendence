08359d7658741b15be058dd96cd974935ecb4fab
bdekonin <bdekonin@student.codam.nl>
2022-11-23 16:26:33 +0100

    changed user endpoint
    - no need to give {userID} anymore

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Body, Controller, Delete, FileTypeValidator, Get, HttpCode, MaxFileSizeValidator, NotFoundException, Param, ParseFilePipe, ParseIntPipe, Patch, Post, Res, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Delete, FileTypeValidator, Get, HttpCode, MaxFileSizeValidator, NotFoundException, Param, ParseFilePipe, ParseIntPipe, Patch, Post, Res, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { User } from "src/entities/User.entity";
 import { UserService } from "./user.service";
 import { ApiTags, ApiOkResponse, ApiNotFoundResponse, ApiParam, ApiBody, ApiNoContentResponse } from "@nestjs/swagger";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { updateUserDto } from "./user.dto";
+import { UserRequest } from "./user.decorator";
+import { AuthenticateGuard } from "src/auth/utils/Guards";
 
 @ApiTags('user')
 @Controller('/user/')
@@ -11,8 +13,9 @@ export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	// General
-	@Get()
+	@Get('all')
 		@ApiOkResponse({ description: 'Returns all users', type: User })
+	@UseGuards(AuthenticateGuard)
 	async getAllUsers()
 	{
 		const users = await this.userService.userRepository.find({
@@ -26,24 +29,24 @@ export class UserController {
 	}
 
 	// Profile settings
-	@Get(':userID/avatar')
+	@Get('avatar')
 		@ApiNotFoundResponse({description: 'User not found'})
 		@ApiOkResponse({ description: 'Returns the image of the userID'})
 		@ApiParam({ name: 'otherID', type: 'number', required: true, description: 'The ID of the user' })
 	async getUserAvatar(
-		@Param('userID', ParseIntPipe) userID: number,
+		@UserRequest() user: User,
 		@Res() res
 	)
 	{
-		return await this.userService.getAvatar(userID, res);
+		return await this.userService.getAvatar(user.id, res);
 	}
-	@Post(':userID/avatar')
+	@Post('avatar')
 		@ApiNotFoundResponse({description: 'User not found'})
 		@ApiOkResponse({ description: 'Returns the image of the userID'})
 		@ApiParam({ name: 'otherID', type: 'number', required: true, description: 'The ID of the user who wants to be followed' })
 	@UseInterceptors(FileInterceptor('image'))
 	async setUserAvatar(
-		@Param('userID', ParseIntPipe) userID: number,
+		@UserRequest() user: User,
 		@UploadedFile(
 			new ParseFilePipe({
 				validators: [
@@ -55,45 +58,62 @@ export class UserController {
 		file: Express.Multer.File
 	)
 	{
-		return this.userService.postAvatar(userID, file.filename);
+		return this.userService.postAvatar(user.id, file.filename);
 	}
-	@Delete(':userID/avatar')
+	@Delete('avatar')
 		@ApiNotFoundResponse({description: 'User not found'})
 		@ApiNoContentResponse({ description: 'Delete current image and reverts to default.jpeg'})
 		@ApiParam({ name: 'otherID', type: 'number', required: true, description: 'The ID of the user who wants to be followed' })
 	@HttpCode(204)
 	async deleteUserAvatar(
-		@Param('userID', ParseIntPipe) userID: number,
+		@UserRequest() user: User,
 	)
 	{
-		return this.userService.deleteAvatar(userID);
+		return this.userService.deleteAvatar(user.id);
 	}
 
 	/* twoFactor */
-	@Get(':userID/twofa/')
+	@Get('twofa/')
 		@ApiNotFoundResponse({description: 'User not found'})
 		@ApiOkResponse({ description: 'Returns if the user has 2fa enables', type: Boolean })
 		@ApiParam({ name: 'otherID', type: 'number', required: true, description: 'The ID of the user who wants to be followed' })
 	async getUsertwoFA(
-		@Param('userID', ParseIntPipe) userID: number,
+		@UserRequest() user: User,
 	)
 	{
-		return await this.userService.getTwoFA(userID);
+		return await this.userService.getTwoFA(user.id);
 	}
 
 	// @Get('/me')
 
 	/* Profile */
-	@Get(':userID')
+	@Get()
 		@ApiNotFoundResponse({description: 'User not found'})
 		@ApiOkResponse({ description: 'Returns everything about the user', type: User })
 		@ApiParam({ name: 'otherID', type: 'number', required: true, description: 'The ID of the user who wants to be followed' })
 	async getUser(
-		@Param('userID', ParseIntPipe) userID: number
+		@UserRequest() user: User
+	): Promise<User> {
+
+		// const user = await this.userService.userRepository.findOne({
+		// 	where: {id: userID},
+		// 	relations: ['membership', 'games_won', "games_lost", 'sentFriendRequests', 'receivedFriendRequests', 'chats']
+		// });
+		// if(!user) {
+		// 	throw new NotFoundException('User Not Found');
+		// }
+		return user;
+	}
+	@Get(':username')
+		@ApiNotFoundResponse({description: 'User not found'})
+		@ApiOkResponse({ description: 'Returns everything about the user', type: User })
+		@ApiParam({ name: 'otherID', type: 'number', required: true, description: 'The ID of the user who wants to be followed' })
+	async getUserbyID(
+		@Param('userID') username: string
 	): Promise<User> {
 
 		const user = await this.userService.userRepository.findOne({
-			where: {id: userID},
+			where: {username: username},
 			relations: ['membership', 'games_won', "games_lost", 'sentFriendRequests', 'receivedFriendRequests', 'chats']
 		});
 		if(!user) {
@@ -101,16 +121,17 @@ export class UserController {
 		}
 		return user;
 	}
-	@Patch(':userID')
+
+	@Patch()
 		@ApiNotFoundResponse({description: 'User not found'})
 		@ApiOkResponse({ description: 'returns the updates user object', type: User })
 		@ApiBody({ type: updateUserDto })
 		@ApiParam({ name: 'otherID', type: 'number', required: true, description: 'The ID of the user who wants to be followed' })
 	async updateUser(
-		@Param('userID', ParseIntPipe) userID: number,
+		@UserRequest() user: User,
 		@Body() body: updateUserDto
 	)
 	{
-		return await this.userService.updateUser(userID, body);
+		return await this.userService.updateUser(user.id, body);
 	}
 }
