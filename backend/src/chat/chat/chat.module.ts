25a3b5ef6ebf57993ff31a1542b74aef6fd3624c
rkieboom <rkieboom@student.codam.nl>
2022-11-23 02:06:56 +0100

    fixed error in backend

--- backend/src/chat/chat/chat.module.ts
+++ backend/src/chat/chat/chat.module.ts
@@ -2,10 +2,5 @@ import { Module } from '@nestjs/common';
 
 @Module({})
 export class ChatModule {
-	imports: [
-		TypeOrmModule.forFeature([Chat, User, Message]),
-		
-	],
-	  controllers: [],
-	  providers: [],
+
 }
