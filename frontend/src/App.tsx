3e8e89b05c6498a1cb72d80b25d8c1f7a8eb9a70
bdekonin <bdekonin@student.codam.nl>
2022-12-12 16:58:40 +0100

    faster ping

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,20 +10,21 @@ import Social from "./components/Socials/Social";
 import Profile from "./components/Profile/Profile";
 import Settings from "./components/Settings/Settings";
 import Chat from "./components/Chat/Chat";
-import React, { useEffect } from "react";
-import { socket, SocketContext } from "./context/socket";
+import React, { useContext, useEffect } from "react";
+import { SocketContext } from "./context/socket";
 import Game from "./components/Game/Game";
 
 const App:React.FC = () =>
 {
 	const minute = 60 * 1000; /* 1 Minute * 1 second */
+	const socket = useContext(SocketContext);
 	document.body.style.background = "#474E68";
 
 	useEffect(() => {
 		// interval
 		const interval = setInterval(() => {
 			socket.emit('ping');
-		}, minute * 5);
+		}, minute * 1);
 
 		// interval.
 	});
