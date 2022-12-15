2ae4db243d78346d51256e6d649d091cc6e08a16
rkieboom <rkieboom@student.codam.nl>
2022-12-15 02:31:53 +0100

    Oops :(

--- frontend/src/components/Socials/Social.tsx
+++ frontend/src/components/Socials/Social.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { FC, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../../styles/socials.css";
 import './style.css'
 
 interface User {
