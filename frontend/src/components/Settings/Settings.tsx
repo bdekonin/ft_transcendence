301b68e83d9068b20cec695501edf0b4b9fb495a
rkieboom <rkieboom@student.codam.nl>
2022-12-21 19:00:44 +0100

    Changed page and made it functional

--- frontend/src/components/Settings/Settings.tsx
+++ frontend/src/components/Settings/Settings.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, LegacyRef, RefObject, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './style.css'
 
@@ -15,55 +15,99 @@ const Settings:React.FC = () => {
 	const navigate = useNavigate();
 	const [user, setUser] = useState<User>();
 	const [userNameDef, setUserNameDef] = useState('');
+	const [originUserName, setOriginUserName] = useState('');
+	const [originAvatar, setOriginAvatar] = useState('');
 	const [avatar, setAvatar] = useState('');
-	// let user: User;
+	const inputRef = useRef<HTMLInputElement | null> (null);
 
 	useEffect(() => {
+		getUserInfo();
+		getAvatar();
+	}, [])
+
+	function getUserInfo() {
 		axios.get('http://localhost:3000/user', {withCredentials: true})
 		.then(elem => {
 			setUser(elem.data);
 			console.log(elem.data);
 			setUserNameDef(elem.data.username);
+			setOriginUserName(elem.data.username);
 		})
 		.catch(err => {
 			console.log(err);
 			navigate('/login');
 		})
+	}
 
+	function getAvatar() {
 		axios.get('http://localhost:3000/user/avatar', {withCredentials: true, responseType: 'blob'})
 		.then(elem => {
 			setAvatar(URL.createObjectURL(elem.data));
+			setOriginAvatar(URL.createObjectURL(elem.data));
 		})
 		.catch(err => {
 			console.log(err);
 		})
-
-	}, [])
-	
-	function enable2FA() {
-
 	}
 
-	function changeAvatar(e: ChangeEvent<HTMLInputElement>) {
+	function onChangeAvatar(e: ChangeEvent<HTMLInputElement>) {
 		const image: File = e.target.files![0];
 		setAvatar(URL.createObjectURL(image));
+	}
+
+	function onRemoveAvatar() {
+		//set Originial avatar
+		setAvatar(originAvatar);
+
+		//Remove selected image
+		if (inputRef.current?.value)
+			inputRef.current.value = "";
+		if (inputRef && inputRef.current)
+			inputRef.current.files = null;
+	}
+
+	function sumbit() {
+		if (userNameDef !== originUserName)
+		{
+			//Update username here
+			axios.patch('http://localhost:3000/user', {username: userNameDef}, {withCredentials: true})
+			.then((res) => {
+				console.log('Succesfully changed name!');
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+		}
+		if (inputRef.current && inputRef.current.value)
+		{
+			//update Avatar here
+			axios.post('http://localhost:3000/user/avatar', {file: inputRef.current.files![0]}, {withCredentials: true, headers: { 'Content-Type': 'multipart/form-data' }})
+			.then((res) => {
+				console.log('Avatar uploaded succesfully!');
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+		}
 	}
 
 	return (
 		<div className="settings">
 			<div className="background"/>
 			<button onClick={() => {navigate('/')}}>Home</button>
-			<img className='header' src={require('./logo.png')}/>
+			<img className='header' src={require('./images/logo.png')}/>
 			<div className="block1">
 				<h2>Username: </h2>
 				<input type="text" value={userNameDef} maxLength={14} onChange={(e) => {setUserNameDef(e.target.value)}}/>
 				<h2>Avatar: </h2>
-				<img className='avatar' src={avatar} />
+				<img className='avatar' src={avatar}/>
 				<br />
-				<input type="file" id="img" name="img" accept="image/*" onChange={changeAvatar}/>
+				<input ref={inputRef} type="file" id="img" name="img" accept="image/*" onChange={onChangeAvatar}/>
 				<h2>2FA</h2>
 				{ user && !user.twofa ? <button>Enable 2FA</button> : <button>Disable 2FA</button>}
-				<button>Update</button>
+				{inputRef.current && inputRef.current.value ? <button onClick={onRemoveAvatar}>Reset avatar</button> : ''}
+				<br />
+				<button onClick={sumbit}>Update</button>
 			</div>
 		</div>
 	);
