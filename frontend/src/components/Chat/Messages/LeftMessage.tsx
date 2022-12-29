3600f193f09d304d232bfe97398d164a06cb5daf
bdekonin <bdekonin@student.codam.nl>
2022-12-29 16:28:57 +0100

    Now prints  ${hours}:${minutes}:${seconds} on messages

--- frontend/src/components/Chat/Messages/LeftMessage.tsx
+++ frontend/src/components/Chat/Messages/LeftMessage.tsx
@@ -7,12 +7,23 @@ const leftMessage: React.FC<{
 	sendTime: string;
 }> = ({ myKey, sender, content, sendTime }) => {
 
+
+	function parseTime() {
+		const time = new Date(Number(sendTime));
+		const hours = time.getHours();
+		const minutes = time.getMinutes();
+		const seconds = time.getSeconds();
+		if (seconds < 10)
+			return `${hours}:${minutes}:0${seconds}`;
+		return `${hours}:${minutes}:${seconds}`;
+	}
+
 	
 	return (
 		<div className="box" key={myKey}>
 			<span className="username-left">{sender}</span>
 			<p>{content}</p>
-			<span className="time-left">{sendTime}</span>
+			<span className="time-left">{parseTime()}</span>
 		</div>
 	);
 }
