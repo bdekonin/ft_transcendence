9ec0d0a03328f75649dece9d2f2b7155b79c9800
bdekonin <bdekonin@student.codam.nl>
2023-01-03 13:12:10 +0100

    Moved game to gamescreen. Removed interval in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,25 +11,15 @@ import Settings from "./components/Settings/Settings";
 import Chat from "./components/Chat/Chat";
 import React, { useContext, useEffect } from "react";
 import { SocketContext } from "./context/socket";
-import Game from "./components/Game/Game";
+import Game from "./components/Gamescreen/Pong/Game";
 import Protected from "./Auth";
 import Login from "./components/Login/Login/Login";
 import Gamescreen from "./components/Gamescreen/Gamescreen";
 
 const App:React.FC = () =>
 {
-	const minute = 60 * 1000; /* 1 Minute * 1 second */
 	const socket = useContext(SocketContext);
 	document.body.style.background = "#474E68";
-
-	useEffect(() => {
-		// interval
-		const interval = setInterval(() => {
-			socket.emit('ping');
-		}, minute * 1);
-
-		// interval.
-	});
 
 	return (
 		<SocketContext.Provider value={socket}>
