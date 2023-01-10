49102a9f96977d1b2cb1beebaf65fda4acd37e71
bdekonin <bdekonin@student.codam.nl>
2023-01-10 15:10:58 +0100

    Game error fixes

--- frontend/src/components/Gamescreen/Pong/Game.tsx
+++ frontend/src/components/Gamescreen/Pong/Game.tsx
@@ -73,7 +73,7 @@ export enum Theme {
 	SPACE,
 }
 
-
+// eslint-disable-next-line
 const Game: React.FC = () => {
 
 	const socket = useContext(SocketContext);
@@ -107,16 +107,16 @@ const Game: React.FC = () => {
 		if (socket) { /* Socket stuff */
 			socket.on('chat/refresh-message', (payload: Message) => {
 				/* Enable notification for that channel */
-				if (payload.parent.type == 'PRIVATE')
+				if (payload.parent.type === 'PRIVATE')
 					showSnackbarNotification(enqueueSnackbar, "New message from " + payload.sender.username, 'info');
 				else
-					showSnackbarNotification(enqueueSnackbar, "New message in groupchat \'" + payload.parent.name + "\'", 'info');
+					showSnackbarNotification(enqueueSnackbar, "New message in groupchat '" + payload.parent.name + "'", 'info');
 			});
 		}
 		return () => {
 			socket.off('chat/refresh-message');
 		}
-	}, [socket]);
+	}, [socket, enqueueSnackbar]);
 
 	useEffect(() => {
 		if (!draw) {
@@ -124,7 +124,7 @@ const Game: React.FC = () => {
 				setDraw(new Draw(canvasRef.current.height, canvasRef.current.width));
 			}
 		}
-	}, [canvasRef]);
+	}, [canvasRef, draw]);
 
 	useEffect(() => {
 		if (location.hash) {
@@ -141,7 +141,7 @@ const Game: React.FC = () => {
 			/* User has invited a game or joined? */
 			const values = location.search.split('=');
 
-			if (values[0] != '?invite')
+			if (values[0] !== '?invite')
 				return ;
 			
 			console.warn(values);
@@ -158,10 +158,10 @@ const Game: React.FC = () => {
 			socket.off("game/spectate");
 			socket.off("game/invite-start");
 		}
-	}, [socket]);
+	}, [socket, location]);
 
 	useEffect(() => {
-		if (state == STATE.WAITING) {
+		if (state === STATE.WAITING) {
 			if (!location.search) {
 				socket.emit("game/waiting");
 			}
@@ -169,7 +169,7 @@ const Game: React.FC = () => {
 		socket.on("game/start", (data: Game) => {
 			setGameState(data);
 			setBall(data.ball)
-			if (state != STATE.SPECTATOR) {
+			if (state !== STATE.SPECTATOR) {
 				setState(STATE.INTRO);
 			}
 		});
@@ -177,34 +177,34 @@ const Game: React.FC = () => {
 			socket.off("game/start");
 			socket.emit("game/leave");
 		};
-	}, [socket]);
+	}, [socket, state, location]);
 
 	useEffect(() => {
 		socket.on('game/rejoin', (data: Game) => {
 			setGameState(data);
 			setBall(data.ball)
-			if (state != STATE.SPECTATOR) {
+			if (state !== STATE.SPECTATOR) {
 				setState(STATE.PLAYING);
 			}
 		});
 		return () => {
 			socket.off("game/rejoin");
 		}
-	}, [socket]);
+	}, [socket, state]);
 
 	useEffect(() => {
 		socket.on("game/update", (data: Game) => {
 			/* Update game state */
 			setGameState(data);
-			if (state != STATE.SPECTATOR) {
-				if (socket.id == data.left.socket || socket.id == data.right.socket)
+			if (state !== STATE.SPECTATOR) {
+				if (socket.id === data.left.socket || socket.id === data.right.socket)
 					setState(STATE.PLAYING);
 			}
 		});
 		return () => {
 			socket.off("game/update");
 		};
-	}, [socket]);
+	}, [socket, state]);
 
 	useEffect(() => {
 		socket.on("game/ball", (data: Ball) => {
@@ -230,7 +230,7 @@ const Game: React.FC = () => {
 	/* Mouse move handler */
 	const mouseMoveHandler = useCallback(
 		(e: MouseEvent) => {
-			if (typeof gameState != "undefined" && state == STATE.PLAYING) {
+			if (typeof gameState != "undefined" && state === STATE.PLAYING) {
 				socket.emit("game/move", {y: e.clientY, id: gameState.id});
 			}
 		},
@@ -249,7 +249,7 @@ const Game: React.FC = () => {
 	// define an effect that decrements the count every second
 	useEffect(() => {
 		const interval = setInterval(() => {
-			if (state == STATE.INTRO) {
+			if (state === STATE.INTRO) {
 				if (count <= 0) {
 					setState(STATE.PLAYING);
 					setCount(10);
@@ -257,7 +257,7 @@ const Game: React.FC = () => {
 				}
 				setCount(count - 1);
 			}
-			if (state == STATE.END) {
+			if (state === STATE.END) {
 				if (count <= 0) {
 					clearInterval(interval);
 					navigate("/");
@@ -278,24 +278,24 @@ const Game: React.FC = () => {
 			return;
 		if (!draw)
 			return;
-		if (state == STATE.WAITING) {
+		if (state === STATE.WAITING) {
 			draw.drawWaiting(theme, canvasRef.current, context.current)
 		}
-		if (state == STATE.INTRO && gameState) {
+		if (state === STATE.INTRO && gameState) {
 			draw.drawIntro(theme, count, socket.id, {
 				context: context.current,
 				canvas: canvasRef.current,
 				gameState: gameState,
 			});
 		}
-		else if ((state == STATE.SPECTATOR || state == STATE.PLAYING) && gameState && ball) {
+		else if ((state === STATE.SPECTATOR || state === STATE.PLAYING) && gameState && ball) {
 			draw.drawPlaying(theme, ball, {
 				context: context.current,
 				canvas: canvasRef.current,
 				gameState: gameState,
 			});
 		}
-		else if (state == STATE.END && gameState && winner) {
+		else if (state === STATE.END && gameState && winner) {
 			draw.drawEnd(theme, count, winner, {
 				context: context.current,
 				canvas: canvasRef.current,
@@ -305,7 +305,7 @@ const Game: React.FC = () => {
 	};
 
 	const update = () => {
-		if ((state == STATE.SPECTATOR || state == STATE.PLAYING) && gameState && ball) {
+		if ((state === STATE.SPECTATOR || state === STATE.PLAYING) && gameState && ball) {
 			//check top canvas bounds
 			if(ball.y < 10){
 				ball.yVel = 1;
@@ -318,13 +318,13 @@ const Game: React.FC = () => {
 
 			//check left canvas bounds
 			if(ball.x < 0){
-				if (state == STATE.PLAYING)
+				if (state === STATE.PLAYING)
 					socket.emit("game/score", {side: "right", id: gameState.id});
 			}
 
 			//check right canvas bounds
 			if(ball.x + ball.width > 700){
-				if (state == STATE.PLAYING)
+				if (state === STATE.PLAYING)
 					socket.emit("game/score", {side: "left", id: gameState.id});
 			}
 
